ff24a47f1c943bca71a232278639ad4e36e1c57a
sugimotoyuuki <kwkwkyy@yahoo.co.jp>
2023-04-30 13:31:33 +0900

    286_fix

--- contest/ABC/286/c.py
+++ contest/ABC/286/c.py
@@ -1,2 +1,18 @@
+from collections import deque
+
 n, a, b = map(int, input().split())
-s = input()
+s = deque(list(input()))
+
+ans = float("inf")
+for i in range(n):
+    value = 0
+    if i != 0:
+        s.append(s.popleft())
+    value += a * i
+    for j in range(n // 2):
+        if s[j] != s[n - j - 1]:
+            value += b
+
+    ans = min(ans, value)
+
+print(ans)
