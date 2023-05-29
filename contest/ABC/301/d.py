ad82457ccf19b834200b4e4b3becf382d657af2f
sugimotoyuuki <kwkwkyy@yahoo.co.jp>
2023-05-30 08:51:35 +0900

    301

--- contest/ABC/301/d.py
+++ contest/ABC/301/d.py
@@ -1,11 +1,23 @@
-s_bin = input()
+s = list(input())
 n = int(input())
 
-if int(s_bin) <= n:
-    print(n)
-    exit()
-
-n_bin = format(n, "b")
-for i, s_dig, n_dig in enumerate(zip(s_bin, n_bin)):
-    if s_dig != n_dig:
-        diff_dig = i
+m = len(s)
+while True:
+    tmp_s = s.copy()
+    exist_q = False
+    for i in range(m):
+        if exist_q and s[i] == "?":
+            tmp_s[i] = "0"
+        elif s[i] == "?":
+            tmp_s[i] = "1"
+            top_num = i
+            exist_q = True
+    if not exist_q:
+        if n >= int("".join(tmp_s), 2):
+            exit(print(int("".join(tmp_s), 2)))
+        else:
+            exit(print(-1))
+    if n >= int("".join(tmp_s), 2):
+        s[top_num] = "1"
+    else:
+        s[top_num] = "0"
